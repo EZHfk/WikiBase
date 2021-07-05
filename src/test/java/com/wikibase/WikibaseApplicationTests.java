6857bca8a5621ce12fe0da3611630a86fa227009
EZHfk <yhou63@wisc.edu>
2021-07-05 04:29:48 -0500

    add git files

---
+++ src/test/java/com/wikibase/WikibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wikibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
