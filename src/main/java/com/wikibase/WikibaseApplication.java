641d28c7e62b140bf37b68437a9954dfee19e731
EZHfk <yhou63@wisc.edu>
2021-07-05 06:06:32 -0500

    Modify Startup Description

--- src/main/java/com/wikibase/WikibaseApplication.java
+++ src/main/java/com/wikibase/WikibaseApplication.java
@@ -1,13 +1,20 @@
 package com.wikibase;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikibaseApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(WikibaseApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(WikibaseApplication.class, args);
+		SpringApplication app = new SpringApplication(WikibaseApplication.class);
+		Environment environment = app.run(args).getEnvironment();
+		LOG.info("Start Successful!");
+		LOG.info("Address: \thttp://127.0.0.1:{}",environment.getProperty("server.port"));
 	}
 
 }
