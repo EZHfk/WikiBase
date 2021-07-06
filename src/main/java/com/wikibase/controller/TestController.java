6e62fde12972c4bbab18eb29e8f16a6bf5d170b5
EZHfk <yhou63@wisc.edu>
2021-07-05 20:35:17 -0500

    Assign config value to controller test

--- src/main/java/com/wikibase/controller/TestController.java
+++ src/main/java/com/wikibase/controller/TestController.java
@@ -1,18 +1,21 @@
 package com.wikibase.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //用来返回字符串；@Controller用来返回页面（前后端分离基本用不到）
 public class TestController {
 
+    @Value("${test.hello:TEST}") //优先读配置项，没有就读：后的默认值
+    private String hello;
     /**
      * GET,POST,PUT,DELETE:增删改查
-     * @return
+     * @return String
      */
     //@RequestMapping(value = "/hello",method = RequestMethod.GET)
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return hello;
     }
 }
