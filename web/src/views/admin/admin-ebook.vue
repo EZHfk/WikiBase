bc0380578edc4116d9c06254251e03f955783b39
EZHfk <yhou63@wisc.edu>
2021-07-08 19:46:27 +0800

    Add Tool for copy record when update Ebook

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -414,7 +414,7 @@
   import { defineComponent, onMounted, ref } from 'vue';
   import axios from 'axios';
   import { message } from 'ant-design-vue';
-  // import {Tool} from "@/util/tool";
+  import {Tool} from "@/util/tool";
 
   export default defineComponent({
     name: 'AdminEbook',
@@ -555,7 +555,7 @@
        */
       const edit = (record: any) => {
         modalVisible.value = true;
-        ebook.value=record;
+        ebook.value=Tool.copy(record);
         // categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
       };
 
